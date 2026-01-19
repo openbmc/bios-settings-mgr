0a023b05f5868716e7846215ff7826a9b5542bd5
Manojkiran Eda <manojkiran.eda@gmail.com>
2026-01-19 18:59:39 +0530

    meson: drop configuration.h generation and usage
    The configuration header was generated but unused.
    Remove configure_file() and the stale include from main.cpp.
    
    Change-Id: Ibe614b9b8dd57d42825f0713464d0220562f0c72
    Signed-off-by: Manojkiran Eda <manojkiran.eda@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,6 @@
  See the License for the specific language governing permissions and
  limitations under the License.
 */
-#include "configuration.h"
 
 #include "config.hpp"
 #include "manager.hpp"
